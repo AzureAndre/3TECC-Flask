f52989cc8755af8b5d5f7d1b47c21b3deb474544
Andre de Beer <andre.debeer@outlook.co.nz>
2026-04-24 13:04:48 +1200

    Befor class 24/4

---
+++ samples/data.py
@@ -0,0 +1,24 @@
+allposts= [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1.',
+        'author': 'Author 1'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2.'
+    },
+    {
+        'title': 'Post 3',
+        'content': 'This is the content of post 3.'
+    },
+    {
+        'title': 'Post 4',
+        'content': 'This is the content of post 4.',
+        'author': 'Author 4'
+    },
+    {
+        'title': 'Post 5',
+        'content': 'This is the content of post 5.'
+    }
+]
